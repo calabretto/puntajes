36713ad99ae67ba55792f4cf25a25fef81922658
Franco Calabretto <francalabretto@gmail.com>
2023-12-12 19:27:16 -0300

    cors

--- src/main/java/imb/lh/puntajes/controller/CarreraController.java
+++ src/main/java/imb/lh/puntajes/controller/CarreraController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import imb.lh.puntajes.entity.Carrera;
 import imb.lh.puntajes.service.ICarrera;
 import jakarta.validation.ConstraintViolationException;
+@CrossOrigin(origins = "http://127.0.0.1:5500") // Reemplaza esto con tu URL de origen
 
 @RestController
 @RequestMapping("/api/v1/carrera") 
