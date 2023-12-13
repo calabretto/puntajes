7f1dcb5e6d6984c012763194c0509e9a0aec1870
Franco Calabretto <francalabretto@gmail.com>
2023-12-13 15:59:19 -0300

    avances

--- src/main/java/imb/lh/puntajes/repository/GrillaTabulacionRepository.java
+++ src/main/java/imb/lh/puntajes/repository/GrillaTabulacionRepository.java
@@ -11,11 +11,11 @@ import imb.lh.puntajes.entity.GrillaTabulacion;
 
 public interface GrillaTabulacionRepository extends JpaRepository<GrillaTabulacion, Integer> {
 
-	@Query("SELECT g.aplicante FROM GrillaTabulacion g WHERE g.aplicante.id = :aplicante AND g.materia.id = :materia AND g.periodo.id = :periodo")
+	@Query("SELECT g FROM GrillaTabulacion g WHERE g.aplicante.id = :aplicante AND g.materia.id = :materia AND g.periodo.id = :periodo")
 	List<GrillaTabulacion> buscarPorIds(@Param("aplicante") Integer aplicante, 
 	                                    @Param("materia") Integer materia, 
 	                                    @Param("periodo") Integer periodo);
 
-	@Query("SELECT g FROM GrillaTabulacion g WHERE g.materia.id = :materia AND g.periodo.id = :periodo")
+	@Query("SELECT g.aplicante FROM GrillaTabulacion g WHERE g.materia.id = :materia AND g.periodo.id = :periodo")
     List<Aplicante> buscarPorMateriaYPeriodo(@Param("materia") Integer materia, @Param("periodo") Integer periodo);
 }
